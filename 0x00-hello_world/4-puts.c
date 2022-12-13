b54f1c0f890043701af1b9cac4289a3488710d58
Waadals <adw9968@gmail.com>
2022-12-12 20:11:27 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*
- * main - print a line of code using  puts
+ * main - print a line of code using  puts*
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
